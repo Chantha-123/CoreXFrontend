71435461bf78aa53d9af3796091c5891937d7d63
ActiveProgrammar <148531059+ActiveProgrammar@users.noreply.github.com>
2025-09-25 23:08:26 +0700

    fix npm

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,9 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '250828',
-    'hash': 'b92f79e2'
-  }
+    'version': '250925',
+    'hash': 'ga4f8febc'
+  },
+  'allow_switching_backend_instance': true
 };
 /* tslint:enable */
